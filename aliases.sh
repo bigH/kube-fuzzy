ba58ba4284d9ac3b900657992505fc1a8e7c2274
Hiren Hiranandani <hiren.hiranandani@chime.com>
2024-03-20 22:31:28 -0700

    init

---
+++ aliases.sh
@@ -0,0 +1,15 @@
+alias k='kubectl-wrapper'
+alias kg='kubectl-wrapper --log-context get'
+alias kd='kubectl-wrapper --log-context describe'
+alias kl='kubectl-wrapper --log-context logs'
+alias kc='kubectl-wrapper config'
+
+alias kcc='kubectl config current-context'
+
+alias kfv='kf get'
+alias kfg='kf get'
+alias kfd='kf describe'
+alias kfe='kf edit'
+
+alias kscn='ksc ; ksn'
+
